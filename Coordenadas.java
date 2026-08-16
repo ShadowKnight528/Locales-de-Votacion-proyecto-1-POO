116f4f841763080498915f32cae7aecb98e92ed5
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-08-15 21:11:56 -0400

    Create Coordenadas class for coordinate representation

---
+++ Coordenadas.java
@@ -0,0 +1,28 @@
+package package_00;
+
+public class Coordenadas {
+	
+	private double x;
+	private double y;
+	
+	public Coordenadas(double x, double y) {
+		setX(x);
+		setY(y);
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+}
