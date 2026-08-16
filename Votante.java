4235b70aef10bab40dcb4455412c0a6e0128aa74
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-08-15 21:14:09 -0400

    Create Votante class extending Ciudadano

---
+++ Votante.java
@@ -0,0 +1,30 @@
+package package_00;
+
+public class Votante extends Ciudadano {
+	
+	private boolean estadoVoto;
+	private Coordenadas residencia;
+	
+	public Votante(String rut, String nombre, Coordenadas residencia) {
+		super(rut, nombre);
+		setEstadoVoto(false);
+		setResidencia(residencia);
+	}
+	
+	public void setEstadoVoto(boolean estadoVoto) {
+		this.estadoVoto = estadoVoto;
+	}
+	
+	public void setResidencia(Coordenadas residencia) {
+		this.residencia = residencia;
+	}
+	
+	
+	public boolean getEstadoVoto() {
+		return estadoVoto;
+	}
+	
+	public Coordenadas getResidencia() {
+		return residencia;
+	}
+}
