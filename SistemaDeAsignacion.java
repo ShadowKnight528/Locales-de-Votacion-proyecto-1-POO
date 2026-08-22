89f44f5283b095b7ea0086168b35fa9c15b4b31d
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-08-22 19:36:46 -0400

    Implement voter assignment system with mesas and sedes

---
+++ SistemaDeAsignacion.java
@@ -0,0 +1,45 @@
+package package_00;
+import java.util.HashMap;
+import java.util.Vector;
+
+public class SistemaDeAsignacion {
+	
+	public boolean asignarMesa(Votante votante, Sede sede) {
+		HashMap<Integer, Mesa> mapaMesas = sede.getMapaMesas();
+		for (Mesa mesa : mapaMesas.values()) {
+			int tamListaVotantes = mesa.getListaVotantes().size();
+			if (tamListaVotantes < mesa.getCapMax()) {
+				if (mesa.agregarVotante(votante) == true) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public void asignarSede(Votante votante, Vector<Sede> vectorSedes) {
+		double menorDistancia = 0;
+		Sede sedeMenorDistancia = null;
+		int aux = 0;
+		for (Sede sede : vectorSedes) {
+			if (sede.getCuposDisponibles() > 0) {
+				if (aux == 0) {
+					menorDistancia = votante.calcularDistanciaASede(sede);
+					sedeMenorDistancia = sede;
+					aux = 1;
+				} else {
+					double distanciaActual = votante.calcularDistanciaASede(sede);
+					if (distanciaActual < menorDistancia) {
+						menorDistancia = distanciaActual;
+						sedeMenorDistancia = sede;
+					}
+				}
+			}
+		}
+		if (sedeMenorDistancia != null) {
+			if (asignarMesa(votante, sedeMenorDistancia) == true) {
+				sedeMenorDistancia.decrementarCuposDisponibles();
+			}
+		}
+	}
+}
