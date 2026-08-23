ffe10b2a1955a36f00e0fe6cefa15b92c149d608
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-08-23 17:11:49 -0400

    Implement interactive menu for voting system

---
+++ MenuInteractivo.java
@@ -0,0 +1,59 @@
+package package_00;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class MenuInteractivo {
+	
+	public void agregarVotanteAMesa(Votante votante, Mesa mesa) {
+		if (votante == null || mesa == null)	{
+			System.out.println("La mesa y/o el votante ingresados no existen");
+			return;
+		} else {
+			if (mesa.agregarVotante(votante) == true) {
+				System.out.println("El votante ha sido añadido con exito a la mesa seleccionada");
+			} else {
+				System.out.println("La mesa que usted ha seleccionado no posee mas cupos");
+			}
+		}
+	}
+	
+	public void agregarMesaASede(Mesa mesa, Sede sede) {
+		if (sede == null) {
+			System.out.println("La sede no existe");
+			return;
+		} else {
+			sede.agregarMesa(mesa);
+			return;
+		}
+	}
+	
+	public void listarVotantesMesa(Mesa mesa) {
+		if (mesa == null) {
+			System.out.println("Esta mesa no existe");
+		} else {
+			ArrayList<Votante> listaVotantes = mesa.getListaVotantes();
+			if (listaVotantes == null) {
+				System.out.println("Esta mesa no tiene votantes");
+			} else {
+				for (Votante votante : listaVotantes) {
+					System.out.println(votante.obtenerDatos());
+				}
+			}
+		}
+	}
+	
+	public void listarMesasSede(Sede sede) {
+		if (sede == null) {
+			System.out.println("La sede no existe");
+		} else {
+			HashMap<Integer, Mesa> mapaMesas = sede.getMapaMesas();
+			if (mapaMesas == null) {
+				System.out.println("La sede no cuenta con mesas disponibles");
+			} else {
+				for (Mesa mesa : mapaMesas.values()) {
+					System.out.println("Numero de mesa: " + mesa.getNumeroMesa() + " Capacidad maxima: " + mesa.getCapMax());
+				}
+			}
+		}
+	}
+}
