f31948c353ca51797b0056c0c62061a5a9ae3d91
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-08-24 18:42:44 -0400

    Enhance documentation for Mesa class and methods
    Added documentation for the Mesa class and its methods, including the agregarVotante method.

--- Mesa.java
+++ Mesa.java
@@ -2,6 +2,16 @@ package package_00;
 import java.util.HashMap;
 import java.util.ArrayList;
 
+/** 
+ * Simula una mesa de votacion de una sede especifica, la clase
+ * cuenta con un numero de mesa, el cual es unico en cada sede y sirve como identificador,
+ * una capacidad maxima que indica cuantos votantes como maximo puede tener asignados esa mesa,
+ * un ArrayList<> de objetos de la Votante que almacena los votantes asignados a la mesa, y
+ * un HashMap<String, Integer> que sirve para que cada mesa lleve su propio conteo de los votos
+ * siendo la clave el nombre del candidato y el valor un entero que corresponde a la cantidad de votos
+ * que lleva dicho candidato
+ */
+
 public class Mesa {
 	
 	private int numeroMesa;
@@ -64,8 +74,22 @@ public class Mesa {
 		return conteoVotos;
 	}
 	
+	/** 
+	 *  El metodo verifica la capacidad maxima de la mesa y la compara con
+	 *  el tamaño del ArrayLista<> de votantes que esta posee para comprobar si la
+	 *  mesa cuenta con mas capacidad para seguir agregando votantes
+	 * 
+	 * @param votante El metodo agrega el objeto de la clase de votante al ArrayList<>
+	 *        de votantes que posee la mesa si se encuentra que el votante ingresado
+	 *        no es null y si la mesa sigue teniendo capacidad para seguir agregando
+	 *        votantes
+	 * @return El metodo retorna true si la mesa posee capacidad para agregar 
+	 * 		   mas votantes y false en caso contrario
+	 * 
+	 * */
+	
 	public boolean agregarVotante(Votante votante) {
-		if (listaVotantes.size() < capMax) {
+		if (listaVotantes.size() < capMax && votante != null) {
 			listaVotantes.add(votante);
 			return true;
 		} else {
