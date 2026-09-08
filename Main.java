98c0e3dcb4f59ee401c355e92c4cccf1451c9ab3
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-09-08 10:44:28 -0300

    Refactor main method to use MenuInteractivo
    Removed console input handling and replaced it with MenuInteractivo for user input.

--- Main.java
+++ Main.java
@@ -7,32 +7,8 @@ import java.io.InputStreamReader;
 public class Main {
 	
     public static void main(String[] args) throws IOException {
-         
-    	BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-    	System.out.println("Seleccione 1 para consola y 2 para ventana");
     	
-    	int opcion = 0;
-    	while(true) {
-    		
-    		try {
-    			opcion = Integer.parseInt(lector.readLine());
-    		} catch (IllegalArgumentException e) {
-    			System.out.println("Debe ingresar un numero entero!");
-    			continue;
-    		}
-    		
-    		switch(opcion) {
-    			case 1:
-    				System.out.println("En progreso, vuelva pronto...");
-    				break;
-    			case 2:
-    				Ventana ventanaPrincipal = new Ventana();
-    				break;
-    			default:
-    				System.out.println("La opción ingresada no es valida, intente ingresando 1 o 2");
-    				continue;
-    		}
-    		break;
-    	}
+    	MenuInteractivo menu = new MenuInteractivo();
+    	menu.leerEntradaUsuario();
     }
 }
