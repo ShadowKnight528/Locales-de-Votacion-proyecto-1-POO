aa872afecf1b9ba74f745e7e04b542d93236b6b1
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-09-08 18:19:12 -0300

    Initialize Vector for Sede and pass to MenuInteractivo

--- Main.java
+++ Main.java
@@ -3,12 +3,14 @@ package package_00;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Vector;
 
 public class Main {
 	
     public static void main(String[] args) throws IOException {
     	
-    	MenuInteractivo menu = new MenuInteractivo();
+    	Vector<Sede> sedes = new Vector<Sede>();
+    	MenuInteractivo menu = new MenuInteractivo(sedes);
     	menu.leerEntradaUsuario();
     }
 }
