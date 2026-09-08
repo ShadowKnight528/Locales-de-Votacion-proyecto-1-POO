d766ae6e89808affdbf84803838d3457aa5e0e2a
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-09-08 10:46:47 -0300

    Add user input handling in MenuInteractivo

--- MenuInteractivo.java
+++ MenuInteractivo.java
@@ -1,8 +1,41 @@
 package package_00;
+
 import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MenuInteractivo {
+	
+    public void leerEntradaUsuario() throws IOException {
+    	BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println("Seleccione 1 para consola y 2 para ventana");
+    	
+    	int opcion = 0;
+    	while(true) {
+    		
+    		try {
+    			opcion = Integer.parseInt(lector.readLine());
+    		} catch (IllegalArgumentException e) {
+    			System.out.println("Debe ingresar un numero entero!");
+    			continue;
+    		}
+    		
+    		switch(opcion) {
+    			case 1:
+    				System.out.println("En progreso, vuelva pronto...");
+    				break;
+    			case 2:
+    				Ventana ventanaPrincipal = new Ventana();
+    				break;
+    			default:
+    				System.out.println("La opción ingresada no es valida, intente ingresando 1 o 2");
+    				continue;
+    		}
+    		break;
+    	}
+    }
 	
 	public void agregarVotanteAMesa(Votante votante, Mesa mesa) {
 		if (votante == null || mesa == null)	{
@@ -239,4 +272,4 @@ public class MenuInteractivo {
 			}
 		}
 	}
-}	
+}
