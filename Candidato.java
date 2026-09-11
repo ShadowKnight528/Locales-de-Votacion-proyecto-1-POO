52ec593bbe8e5887c213a6f73b48b256c837d4dc
ShadowKnight528 <123881926+ShadowKnight528@users.noreply.github.com>
2026-09-11 13:34:09 -0300

    Add serialVersionUID to Candidato class

--- Candidato.java
+++ Candidato.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class Candidato extends Ciudadano implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	private String partido;
 	
 	public Candidato(String rut, String nombre, String partido) {
